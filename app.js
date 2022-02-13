2354bcfc31af7e3beb00556bd348ab1ab95a8457
Fokhrul Islam <shahidcse12@gmail.com>
2022-02-13 19:49:25 +0600

    sub-total-added

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ function updateProductNumber(product, price, isIncreasing){
     productInput.value = productNumber;
     const productTotal = document.getElementById( product+ '-total');
     productTotal.innerText = productNumber * price;
+    //Calculate Total
+    calculateTotal();
 
 }
+
+
 // Phone Increase Decrease
 
 document.getElementById('phone-plus').addEventListener('click',function(){
@@ -20,6 +24,25 @@ document.getElementById('phone-plus').addEventListener('click',function(){
 document.getElementById('phone-minus').addEventListener('click',function(){
     updateProductNumber('phone',1219, false);
 });
+
+function getInputValue(product){
+    const productInput = document.getElementById(product+ '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal(){
+    const phoneTotal = getInputValue('phone') *1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal /10;
+    const totalPrice = subTotal + tax;
+
+    //Update on The HTML
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('total-tax').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+}
 
 
 // case increase decrease
